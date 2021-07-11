cc3b6d774ea80245e4b7b34f7f286091c4193298
ihatefrogg <86327485+ihatefrogg@users.noreply.github.com>
2021-07-11 14:04:52 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+track = loadImage("path.png");
+var track;
+function preload(){
+  //pre-load images
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+}
+
+function draw() {
+  background(0);
+
+}
